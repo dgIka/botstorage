17bc753552b8f78e20c3bcab0800c81b98a6bbb6
Ika <rexar.ika@gmail.com>
2025-11-26 19:26:21 +0300

    added bot, lf4j, lombok

---
+++ dispatcher/src/main/java/org/dgika/controller/TelegramBot.java
@@ -0,0 +1,38 @@
+package org.dgika.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+@Slf4j
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${bot.token}")
+    private String token;
+
+    @Value("${bot.name}")
+    private String name;
+
+
+
+    @Override
+    public String getBotUsername() {
+        return name;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var originalMessage = update.getMessage();
+        log.debug(originalMessage.getText());
+
+    }
+}
