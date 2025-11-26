01a494199c3ac3097614e212b5c39c00d9acfda7
Ika <rexar.ika@gmail.com>
2025-11-26 19:49:10 +0300

    bot answering

--- dispatcher/src/main/java/org/dgika/controller/TelegramBot.java
+++ dispatcher/src/main/java/org/dgika/controller/TelegramBot.java
@@ -5,7 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.bots.DefaultAbsSender;
 
 @Component
 @Slf4j
@@ -34,5 +37,20 @@ public class TelegramBot extends TelegramLongPollingBot {
         var originalMessage = update.getMessage();
         log.debug(originalMessage.getText());
 
+        var response = new SendMessage();
+        response.setChatId(update.getMessage().getChatId().toString());
+        response.setText("Hello from bot");
+        sendAnswerMessage(response);
+
+    }
+
+    public void sendAnswerMessage(SendMessage message)  {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e.getMessage());
+            }
+        }
     }
 }
