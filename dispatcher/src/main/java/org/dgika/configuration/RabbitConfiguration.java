cac808a9673f3ca34a61092a8737c30821824584
Ika <rexar.ika@gmail.com>
2025-12-01 00:29:25 +0300

    rabbit worked

---
+++ dispatcher/src/main/java/org/dgika/configuration/RabbitConfiguration.java
@@ -0,0 +1,22 @@
+package org.dgika.configuration;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.dgika.model.RabbitQueue.TEXT_MESSAGE_UPDATE;
+
+@Configuration
+public class RabbitConfiguration {
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Queue textMessageQueue() {
+        return new Queue(TEXT_MESSAGE_UPDATE);
+    }
+}
--- dispatcher/src/main/java/org/dgika/service/impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/org/dgika/service/impl/UpdateProducerImpl.java
@@ -2,6 +2,7 @@ package org.dgika.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import org.dgika.service.UpdateProducer;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -9,8 +10,15 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 @Service
 @Slf4j
 public class UpdateProducerImpl implements UpdateProducer {
+    private final RabbitTemplate rabbitTemplate;
+
+    public UpdateProducerImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.debug(update.getMessage().toString());
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
