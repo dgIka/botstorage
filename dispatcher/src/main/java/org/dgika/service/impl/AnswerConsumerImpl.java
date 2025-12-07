6f7c6387f9f85a1db11ad8fee5b09a3e8d0f56e6
Ika <rexar.ika@gmail.com>
2025-12-08 01:17:10 +0300

    fixes

--- dispatcher/src/main/java/org/dgika/service/impl/AnswerConsumerImpl.java
+++ dispatcher/src/main/java/org/dgika/service/impl/AnswerConsumerImpl.java
@@ -1,18 +1,19 @@
 package org.dgika.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.dgika.controller.UpdateController;
 import org.dgika.service.AnswerConsumer;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 import static org.dgika.model.RabbitQueue.ANSWER_MESSAGE;
 
+@Service
+@RequiredArgsConstructor
 public class AnswerConsumerImpl implements AnswerConsumer {
-    private UpdateController updateController;
+    private final UpdateController updateController;
 
-    public AnswerConsumerImpl(UpdateController updateController) {
-        this.updateController = updateController;
-    }
 
     @Override
     @RabbitListener(queues = ANSWER_MESSAGE)
